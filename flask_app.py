ab276070cc7d02244f851c29aebea77d6791112c
AhmedGamal7207 <92176653+AhmedGamal7207@users.noreply.github.com>
2024-12-22 07:18:55 +0200

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -2,15 +2,22 @@ from flask import Flask, render_template, request, send_file, url_for
 import os
 from models.tacotron2 import Tacotron2Wave
 from models.fastpitch import FastPitch2Wave
+import torch
+from scipy.io.wavfile import write
 
 app = Flask(__name__)
 
 # Load models
 models = {
-    "FastPitch2Wave Model 1": FastPitch2Wave('checkpoints/exp_fp_adv_40_epoch/states.pth'),
-    "FastPitch2Wave Model 2": FastPitch2Wave('checkpoints/exp_fp_adv/states.pth'),
-    "Tacotron2Wave Model 1": Tacotron2Wave('checkpoints/exp_tc2/states_450.pth'),
-    "Tacotron2Wave Model 2": Tacotron2Wave('pretrained/tacotron2_ar_mse.pth')
+    "Pretrained Tacotron2 mse Model": Tacotron2Wave('pretrained/tacotron2_ar_mse.pth'),
+    "Pretrained Tacotron2 adv Model": Tacotron2Wave('pretrained/tacotron2_ar_adv.pth'),
+    "Pretrained FastPitch mse Model": Tacotron2Wave('pretrained/fastpitch_ar_mse.pth'),
+    "Pretrained FastPitch adv Model": Tacotron2Wave('pretrained/fastpitch_ar_adv.pth'),
+
+    "Our Tacotron2 12 Epoch": Tacotron2Wave('checkpoints/exp_tc2/states_450.pth'),
+    "Our FastPitch 12 Epoch": FastPitch2Wave('checkpoints/exp_fp_adv/states.pth'),
+    "Our FastPitch 40 Epoch": FastPitch2Wave('checkpoints/exp_fp_adv_40_epoch/states.pth')
+ 
 }
 
 @app.route('/')
@@ -33,11 +40,24 @@ def execute():
     model.cuda()
     # Generate the wave
     wave = model.tts(text, vowelizer='shakkelha')
+    
+    # Assuming wave is a PyTorch tensor, convert it to a NumPy array
+    wave_np = wave.cpu().numpy() if wave.is_cuda else wave.numpy()
 
+    # Define the sample rate (e.g., 22050 Hz in your case)
+    sample_rate = 22050
+
+    # Normalize the audio data to ensure it's within the range [-1, 1]
+    wave_np = wave_np / max(abs(wave_np))
+
+    # Convert to 16-bit PCM format for saving as WAV
+    wave_int16 = (wave_np * 32767).astype('int16')
+    
     # Save the wave to a file
     output_path = "static/output.wav"
-    wave.save(output_path)
-
+    # Save the waveform to a .wav file
+    write(output_path, sample_rate, wave_int16)
+    
     # Return the index page with the audio URL
     audio_url = url_for('static', filename='output.wav')
     return render_template('index.html', models=models.keys(), audio_url=audio_url)
